b26dfce606062d05a9f821c3c5e1d5d5423d092f
Christoph Schmidt <45205192+ChristophSchmidt1203@users.noreply.github.com>
2020-02-05 16:40:54 +0100

    do it

--- bots/bot.js
+++ bots/bot.js
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler, TurnContext, ConversationState, MemoryStorage, UserState } = require('botbuilder');
+const { ActivityHandler, TurnContext, TeamsInfo, ConversationState, MemoryStorage, UserState } = require('botbuilder');
 
 class EchoBot extends ActivityHandler {
 	constructor() {
