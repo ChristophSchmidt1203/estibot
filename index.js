70a16776df21f941aa8a5b65ad7fc2d66f032fc0
Christoph Schmidt <45205192+ChristophSchmidt1203@users.noreply.github.com>
2020-02-05 17:26:47 +0100

    do it

--- index.js
+++ index.js
@@ -20,47 +20,55 @@ dotenv.config({ path: ENV_FILE });
 // Create HTTP server
 const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
-    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
+	console.log(`\n${server.name} listening to ${server.url}`);
+	console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+	console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
 });
 
 // Create adapter.
 // See https://aka.ms/about-bot-adapter to learn more about how bots work.
 const adapter = new BotFrameworkAdapter({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    channelService: process.env.ChannelService,
-    openIdMetadata: process.env.BotOpenIdMetadata
+	appId: process.env.MicrosoftAppId,
+	appPassword: process.env.MicrosoftAppPassword,
+	channelService: process.env.ChannelService,
+	openIdMetadata: process.env.BotOpenIdMetadata
 });
 
 // Catch-all for errors.
 adapter.onTurnError = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
-    // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights.
-    console.error(`\n [onTurnError] unhandled error: ${ error }`);
+	// This check writes out errors to console log .vs. app insights.
+	// NOTE: In production environment, you should consider logging this to Azure
+	//       application insights.
+	console.error(`\n [onTurnError] unhandled error: ${error}`);
 
-    // Send a trace activity, which will be displayed in Bot Framework Emulator
-    await context.sendTraceActivity(
-        'OnTurnError Trace',
-        `${ error }`,
-        'https://www.botframework.com/schemas/error',
-        'TurnError'
-    );
+	// Send a trace activity, which will be displayed in Bot Framework Emulator
+	await context.sendTraceActivity(
+		'OnTurnError Trace',
+		`${error}`,
+		'https://www.botframework.com/schemas/error',
+		'TurnError'
+	);
 
-    // Send a message to the user
-    await context.sendActivity('The bot encounted an error or bug.');
-    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+	// Send a message to the user
+	await context.sendActivity('The bot encounted an error or bug.');
+	await context.sendActivity('To continue to run this bot, please fix the bot source code.');
 };
 
+// Define state store for your bot.
+// See https://aka.ms/about-bot-state to learn more about bot state.
+const memoryStorage = new MemoryStorage();
+
+// Create conversation and user state with in-memory storage provider.
+const conversationState = new ConversationState(memoryStorage);
+const userState = new UserState(memoryStorage);
+
 // Create the main dialog.
-const myBot = new EchoBot();
+const myBot = new EchoBot(conversationState, userState);
 
 // Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
-        // Route to main dialog.
-        await myBot.run(context);
-    });
+	adapter.processActivity(req, res, async (context) => {
+		// Route to main dialog.
+		await myBot.run(context);
+	});
 });
